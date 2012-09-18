205f5c2d174cf8dedcca6039b73a5487e11ceda2
Daniel Leung <daniel.leung@intel.com>
2012-09-18 16:49:19 -0700

    Ability to disable input devices due to tablet mode changes
    Adds the ability to disable certain input devices when going into
    or out of tablet mode.
    
    This fixes an issue where the touchpad is too sensitive and giving
    phantom input events, due to the back of screen 'barely' touching it.
    
    Due to the current lack of interest in convertible devices
    on AOSP, this patch is not recommended for upstreaming.
    
    Category: Device Enablement
    Domain: IO
    Upstream-Candidate: no
    
    Change-Id: I497fea885d30d5f96eed1d2b63669991d38e37fa
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Conflicts:
    
    	core/res/res/values/arrays.xml
    	core/res/res/values/symbols.xml

--- services/jni/com_android_server_input_InputManagerService.cpp
+++ services/jni/com_android_server_input_InputManagerService.cpp
@@ -85,6 +85,7 @@ static struct {
     jmethodID getPointerIcon;
     jmethodID getKeyboardLayoutOverlay;
     jmethodID getDeviceAlias;
+    jmethodID setInputDeviceDisabled;
 } gServiceClassInfo;
 
 static struct {
@@ -1292,6 +1293,13 @@ static void nativeMonitor(JNIEnv* env, jclass clazz, jint ptr) {
     im->getInputManager()->getDispatcher()->monitor();
 }
 
+static void nativeSetInputDeviceDisabled(JNIEnv* env,
+        jclass clazz, jint ptr, jint deviceId, jboolean disabled) {
+    NativeInputManager* im = reinterpret_cast<NativeInputManager*>(ptr);
+
+    im->getInputManager()->getReader()->setInputDeviceDisabled(deviceId, disabled);
+}
+
 // ----------------------------------------------------------------------------
 
 static JNINativeMethod gInputManagerMethods[] = {
@@ -1346,6 +1354,8 @@ static JNINativeMethod gInputManagerMethods[] = {
             (void*) nativeDump },
     { "nativeMonitor", "(I)V",
             (void*) nativeMonitor },
+    { "nativeSetInputDeviceDisabled", "(IIZ)V",
+            (void*) nativeSetInputDeviceDisabled },
 };
 
 #define FIND_CLASS(var, className) \
