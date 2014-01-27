efd57ee976693e1e2b66bb33ec3b053e44845555
Constantin Musca <constantin.musca@intel.com>
2014-01-27 14:55:37 +0200

    REVERTME: Enable adb over network on user builds
    Allow the user to enable adbd over network by using the
    Settings.Secure.ADB_PORT option which sets the service.adb.tcp.port
    system property accordingly.
    
    Includes:
    - ADB Over Network, integration of the adb-host mode (already present)
      The feature can be used/tested without the Settings part:
    
        setprop service.adb.tcp.port 5555
    
      Note: This ADB setting is not persistent (for security purpose) and
      requires init.rc event implementation like this : http://bit.ly/AdbTcpIP
    
      Author: Tanguy Pruvot
      Id: I5c61a53948349c785356cb5aae165110d75e3074
    
    Issue: APDEV-1285
    Tracked-by: APDEV-1326
    Category: AOSP Improvement
    Domain: AOSP-framework
    Origin: Cyanogenmod
    Upstream-Candidate: Yes
    Depends-Change-Id: I43dc6518b7d943a6103cd5a23a6f6ef84739e387
    Change-Id: Id34045b03a8804bf17697dde7bf6f9e8559c0481
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-6634
    Integrated-by: Xiaobing Feng <xiaobing.feng@windriver.com
    Signed-off-by: Constantin Musca <constantin.musca@intel.com>
    Reviewed-on: https://android.intel.com/337590
    Reviewed-by: Pircalabu, Petre Ovidiu <petre.ovidiu.pircalabu@intel.com>
    Tested-by: Pircalabu, Petre Ovidiu <petre.ovidiu.pircalabu@intel.com>
    Reviewed-by: Serban, Mihai <mihai.serban@intel.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3488,6 +3488,12 @@ public final class Settings {
         @Deprecated
         public static final String ADB_ENABLED = Global.ADB_ENABLED;
 
+       /**
+         * The TCP/IP port to run ADB on, or -1 for USB
+         * @hide
+         */
+        public static final String ADB_PORT = "adb_port";
+
         /**
          * Setting to allow mock locations and location provider status to be injected into the
          * LocationManager service for testing purposes during application development.  These
